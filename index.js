6573c0c16f27240a23a1b8d28a22b0816c1148ef
03sid-sharma <03sid.sharma@gmail.com>
2021-04-11 15:05:13 +0530

    cases implementation

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const fetchBtn = document.getElementById("submit");
+const btn = document.getElementById("submit");
 
 
-fetchBtn.addEventListener("click", buttonclickhandler);
+btn.addEventListener("click", apiCall);
 
 
 
-function buttonclickhandler() {
+function apiCall() {
     const dateOne = document.getElementById('start').value;
     const dateTwo = document.getElementById('end').value;
     const country = document.getElementById('country').value;
 
-    // Instantiate an new XHR Object
     const xhr = new XMLHttpRequest();
 
-
-    // Open an obejct (GET/POST, PATH,
-    // ASYN-TRUE/FALSE)
     xhr.open("GET",
         `https://api.covid19api.com/country/${country}?from=${dateOne}T00:00:00Z&to=${dateTwo}T00:00:00Z`,
         true);
 
-    // When response is ready
     xhr.onload = function() {
         if (this.status === 200) {
 
-            // Changing string data into JSON Object
-            obj = JSON.parse(this.responseText);
+            var obj = JSON.parse(this.responseText);
             console.log(obj);
-            // Getting the ul element
             let list = document.getElementById("list");
-            str = ""
-            for (key in obj.data) {
-                str += `<li>${obj.data[key].cases}</li>`;
+            var confirm = "";
+            var active = "";
+            var die = "";
+
+            for (let index = 0; index < obj.length; index++) {
+                const element = obj[index];
+                confirm += `<li>${element.Confirmed}</li>`;
+                active += `<li>${element.Active}</li>`;
+                die += `<li>${element.deaths}</li>`;
+
             }
-            list.innerHTML = str;
-        } else {
-            console.log("File not found");
-        }
+            list.innerHTML += confirm;
+            list.innerHTML += active;
+            list.innerHTML += die;
+        } else
+            console.log("404 not found");
     }
 
-    // At last send the request
     xhr.send();
 }
